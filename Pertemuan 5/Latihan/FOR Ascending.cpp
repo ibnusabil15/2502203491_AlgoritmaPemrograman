dbd834b2255e1604ef56412980c3bb2e32aa2969
ibnusabil15 <ibnusabil5202@gmail.com>
2025-10-23 23:38:03 +0700

    Add files via upload

---
+++ Pertemuan 5/Latihan/FOR Ascending.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int awal, akhir;
+	
+	cout << "Mulai dari = ";
+	cin >> awal;
+	
+	cout << "Berakhir di = ";
+	cin >> akhir;
+	
+	for(int a = awal; a <= akhir; a++)
+	{
+		cout << a << ". i'm happy"<<endl;
+	}
+	
+	return 0;
+}
+	
+
+
+
+
+
+
+
+
