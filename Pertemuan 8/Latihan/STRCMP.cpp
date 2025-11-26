6df6c34e57f5fa0e6c2a6b55f219a717e709735a
ibnusabil15 <ibnusabil5202@gmail.com>
2025-11-27 00:56:18 +0700

    Add files via upload

---
+++ Pertemuan 8/Latihan/STRCMP.cpp
@@ -0,0 +1,28 @@
+#include <iostream>>
+#include <fstream>
+using namespace std;
+
+int main()
+{
+    
+    ofstream myfile;
+    myfile.open("Test.exe", ios::app); 
+
+    cout << "OPERASI FILE OFSTREAM" << endl;
+    cout << "---------------------" << endl;
+
+    if (!myfile.fail()) 
+    {
+       
+        myfile << "Belajar OPERASI FILE OFSTREAM" << endl;
+        myfile.close();
+        cout << "Text telah ditulis ke dalam File" << endl;
+    } 
+    else 
+    {
+       
+        cout << "Gagal membuka file!" << endl;
+    }
+    
+    return 0;
+}
