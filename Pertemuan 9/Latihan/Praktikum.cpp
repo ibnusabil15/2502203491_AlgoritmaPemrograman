8e3c956d3bfc0ee0afede61bc47b9523f8f6f4c9
ibnusabil15 <ibnusabil5202@gmail.com>
2025-12-19 14:45:11 +0700

    Add files via upload

---
+++ Pertemuan 9/Latihan/Praktikum.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int matrix[3][3];
+
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            cout << "Masukkan nilai matrix [" << i << "][" << j << "]: ";
+            cin >> matrix[i][j];
+        }
+    }
+
+    cout << "\nHasil Matrix:\n";
+
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            cout << matrix[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
+
